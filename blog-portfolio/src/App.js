69b3b8b76e53ae7ee80d444d5d1b73195f249a9a
Róbert <robertvalcsak93@gmail.com>
2023-03-08 20:27:28 +0100

    background image added

--- blog-portfolio/src/App.js
+++ blog-portfolio/src/App.js
@@ -4,17 +4,24 @@ import Create from "./components/Create";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Wholeblog from "./components/Wholeblog";
+import image from "./images/background.jpg";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+        }}>
         <Navbar />
         <div className="content-list">
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
-            <Route path="/blogs/:id" element={<Wholeblog/>}/>
+            <Route path="/blogs/:id" element={<Wholeblog />} />
           </Routes>
         </div>
       </div>
